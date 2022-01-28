ef0784fdfaf389f17fdc0fc0cb90ffad0bf9d1c9
Ryan Hunter <hunterryan0000@yahoo.com>
2022-01-28 13:36:05 -0600

    created first class

---
+++ Exercise/src/main/java/com/techelevator/Deparment.java
@@ -0,0 +1,31 @@
+package com.techelevator;
+
+public class Deparment {
+
+    
+
+        private int departmentId;
+        private String name;
+
+        public int getDepartmentId() {
+            return departmentId;
+        }
+
+        public void setDepartmentId(int departmentId) {
+            this.departmentId = departmentId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        public Deparment (int departmentId, String name){
+            this.departmentId=departmentId;
+            this.name=name;
+        }
+}
